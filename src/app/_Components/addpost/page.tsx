5828ea617bfec5d727bcd661b81c611b2b7c826c
Alzahraa Saied <zahraasaied592@gmail.com>
2024-07-02 15:58:09 +0300

    Fixed errors made in code

--- src/app/_Components/addpost/page.tsx
+++ src/app/_Components/addpost/page.tsx
@@ -1,59 +1,56 @@
 import { addPost } from '@/lib/postsSlice';
 import { TextareaAutosize } from '@mui/base';
-import { Box, Button,  Typography } from '@mui/material';
-import createTypography from '@mui/material/styles/createTypography';
+import { Box, Button, Typography } from '@mui/material';
 import Link from 'next/link';
 import { useDispatch } from 'react-redux';
+import React from 'react';
 
-export default function AddPost(){
-    const dispatch = useDispatch<any>();
+export default function AddPost() {
+  const dispatch = useDispatch<any>();
 
-    function handleSubmit(e:Event){
-        e.preventDefault();
-        console.log(e.target.body.value);
-        console.log(e.target.image.files[0]);
-        
-        let body= e.target.body.value;
-        let image = e.target.image.files[0];
-        let formData=new FormData();
-        formData.append('body',body);
-        formData.append('image',image);
-        dispatch(addPost(formData));
-       
-        
-        
-    }
-    return(<>
-
-    <form style={{ width: "80%", margin: "10px auto" }} onSubmit={handleSubmit}>
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
     
+    const form = e.target as HTMLFormElement;
+    
+    const body = form.body.value;
+    const image = form.image.files[0];
+    
+    const formData = new FormData();
+    formData.append('body', body);
+    formData.append('image', image);
+    
+    dispatch(addPost(formData));
+  }
+
+  return (
+    <>
+      <form style={{ width: "80%", margin: "10px auto" }} onSubmit={handleSubmit}>
         <Typography variant="h5" gutterBottom>Create a New Post</Typography>
-        <TextareaAutosize name='body'
-          style={{ width: "100%",  padding: 10, fontSize: 16, borderRadius:"5"}}
+        <TextareaAutosize
+          name="body"
+          style={{ width: "100%", padding: 10, fontSize: 16, borderRadius: "5" }}
           minRows={6}
           maxRows={10}
           placeholder="What's on your mind?"
         />
-        <input className='btn' name='image' type="file"/>
+        <input className='btn' name='image' type="file" />
         <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <Button
-        type="submit"
-        variant="contained"
-        color="primary"
-        style={{ marginTop: 10 }}
-      >
-        Post
-      </Button>
-      <Link href="/posts" passHref>
-        <Button variant="contained" color="primary" style={{ marginTop: 20 }}>
-          View Your Timeline
-        </Button>
-      </Link>
-    </Box>
-       
-
-
-    </form>
-
-    </>)
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            style={{ marginTop: 10 }}
+          >
+            Post
+          </Button>
+          <Link href="/posts" passHref>
+            <Button variant="contained" color="primary" style={{ marginTop: 20 }}>
+              View Your Timeline
+            </Button>
+          </Link>
+        </Box>
+      </form>
+    </>
+  );
 }
--- src/app/_Components/navbar/page.tsx
+++ src/app/_Components/navbar/page.tsx
@@ -8,13 +8,11 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
-import Link from 'next/link'; // Import Link from Next.js
-import SinglePost from '../singlepost/page';
+import Link from 'next/link'; 
 import { useSelector } from 'react-redux';
 
 const pages = [
